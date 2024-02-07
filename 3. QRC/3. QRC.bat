e3caad95362eeea900e03298321be4c11504a298
Andrey Mirskiy <35451244+AMirskiy@users.noreply.github.com>
2024-02-07 17:58:28 +0100

    Repo restructuring

--- 3. QRC/3. QRC.bat
+++ 3. QRC/3. QRC.bat
@@ -1,6 +1,6 @@
 SET JMETER_PATH=C:\apache-jmeter-5.6.2\bin
-SET USER_PROPERTIES=user.properties
-SET TEST_PLAN_PATH=Sample Test Plans\Basic Configuration
-SET TEST_PLAN=Basic Configuration.jmx
+SET USER_PROPERTIES=..\user.properties
+SET TEST_PLAN_PATH=.
+SET TEST_PLAN=3. QRC.jmx
 
 call "%JMETER_PATH%\jmeter-t.cmd" ".\%TEST_PLAN_PATH%\%TEST_PLAN%" -p "%~dp0\%USER_PROPERTIES%"
